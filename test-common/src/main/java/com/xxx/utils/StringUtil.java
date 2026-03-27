6d10a81cc1e3b2c2e16c1541e01406ccfd5881b0
kedaya572 <kedaya572@gmail.com>
2026-03-27 21:28:58 +0900

    int

--- test-common/src/main/java/com/xxx/utils/StringUtil.java
+++ test-common/src/main/java/com/xxx/utils/StringUtil.java
@@ -10,13 +10,13 @@ public class StringUtil {
     private StringUtil() {}
 
     /**
-     * 判断字符串是否为空（null 或 length == 0）
+     * 判断字符串是否为空（null 或空串）
      *
      * @param str 字符串
      * @return 是否为空
      */
     public static boolean isEmpty(String str) {
-        return str == null || str.length() == 0;
+        return str == null || str.isEmpty();
     }
 
     /**
@@ -49,12 +49,7 @@ public class StringUtil {
         if (str == null) {
             return true;
         }
-        for (int i = 0; i < str.length(); i++) {
-            if (!Character.isWhitespace(str.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
+        return str.isBlank();
     }
 
     /**
@@ -65,5 +60,110 @@ public class StringUtil {
      */
     public static boolean isNotBlank(String str) {
         return !isBlank(str);
+    }
+
+    /**
+     * 驼峰转下划线
+     *
+     * <p>如：userName → user_name, XMLParser → xml_parser</p>
+     *
+     * @param str 驼峰格式字符串
+     * @return 下划线格式字符串
+     */
+    public static String camelToUnderline(String str) {
+        if (str == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isUpperCase(c)) {
+                // 处理连续大写：如果是连续大写字母的最后一个，且后面还有小写字母，则需要转换
+                if (i > 0 && !Character.isUpperCase(str.charAt(i - 1))) {
+                    sb.append('_');
+                } else if (i > 0 && i < str.length() - 1 
+                           && Character.isUpperCase(str.charAt(i))
+                           && Character.isLowerCase(str.charAt(i + 1))) {
+                    // XMLParser 情况：P 后面是小写，需要在 P 之前加 _
+                    sb.append('_');
+                }
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 下划线转驼峰
+     *
+     * <p>如：user_name → userName, _user_name → _userName</p>
+     *
+     * @param str 下划线格式字符串
+     * @return 驼峰格式字符串
+     */
+    public static String underlineToCamel(String str) {
+        if (str == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        boolean upperNext = false;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '_') {
+                upperNext = true;
+            } else {
+                if (upperNext) {
+                    sb.append(Character.toUpperCase(c));
+                    upperNext = false;
+                } else {
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 安全转换为字符串
+     *
+     * <p>null 返回空字符串 ""，数组类型返回 Arrays.toString() 格式</p>
+     *
+     * @param obj 对象
+     * @return 字符串
+     */
+    public static String toString(Object obj) {
+        if (obj == null) {
+            return "";
+        }
+        if (obj instanceof Object[]) {
+            return java.util.Arrays.toString((Object[]) obj);
+        }
+        if (obj instanceof byte[]) {
+            return java.util.Arrays.toString((byte[]) obj);
+        }
+        if (obj instanceof short[]) {
+            return java.util.Arrays.toString((short[]) obj);
+        }
+        if (obj instanceof int[]) {
+            return java.util.Arrays.toString((int[]) obj);
+        }
+        if (obj instanceof long[]) {
+            return java.util.Arrays.toString((long[]) obj);
+        }
+        if (obj instanceof float[]) {
+            return java.util.Arrays.toString((float[]) obj);
+        }
+        if (obj instanceof double[]) {
+            return java.util.Arrays.toString((double[]) obj);
+        }
+        if (obj instanceof boolean[]) {
+            return java.util.Arrays.toString((boolean[]) obj);
+        }
+        if (obj instanceof char[]) {
+            return java.util.Arrays.toString((char[]) obj);
+        }
+        return obj.toString();
     }
 }
