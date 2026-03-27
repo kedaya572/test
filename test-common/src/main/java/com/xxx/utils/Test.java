a4f70b3d81289f931f625911890697747d4409ae
kedaya572 <kedaya572@gmail.com>
2026-03-27 13:24:52 +0900

    feat: 新增 Test.java 测试类

---
+++ test-common/src/main/java/com/xxx/utils/Test.java
@@ -0,0 +1,18 @@
+package com.xxx.utils;
+
+/**
+ * 测试类
+ *
+ * @author shuimen
+ */
+public class Test {
+
+    /**
+     * 程序入口
+     *
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
